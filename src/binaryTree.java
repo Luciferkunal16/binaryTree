9ec9deb4ecee817a8e8ec3475dae0bdae8a5bbb4
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-11-09 20:05:21 +0530

    ADDED UC3 : Search Element in Binary Seacrh

--- src/binaryTree.java
+++ src/binaryTree.java
@@ -20,22 +20,36 @@ public class binaryTree {
 
 		return current;
 	}
-	/* Adding node with help of recurssion from the root Node
+	/*
+	 * Adding node with help of recurssion from the root Node
 	 */
-	 
-	
-	
+
 	public void add(int value) {
-	    root = addRecursive(root, value);
+		root = addRecursive(root, value);
 	}
+
 	int getSizeRecursive(binaryTreeNode current) {
-		return current == null ? 0:1 + this.getSizeRecursive(current.left )  +this .getSizeRecursive(current.right);
-		
+		return current == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
+
 	}
-	int getSize()
-	{
-	return this.getSizeRecursive(root);
-		
-	
+
+	int getSize() {
+		return this.getSizeRecursive(root);
+
+	}
+// 
+	private boolean searchRecursively(binaryTreeNode current, int value) {
+		if (current == null) {
+			return false;
+		}
+		if (value == current.value) {
+			return true;
+		}
+		return value < current.value ? searchRecursively(current.left, value)
+				: searchRecursively(current.right, value);
+	}
+
+	public boolean search(int value) {
+		return searchRecursively(root, value);
 	}
 }
