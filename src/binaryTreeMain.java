dfb014cfa4b0e3ad7abce8084008980f7481d86e
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-11-09 19:41:31 +0530

    Added UC1: Inserting Elements in Binary Tee

---
+++ src/binaryTreeMain.java
@@ -0,0 +1,14 @@
+package binaryTree;
+
+public class binaryTreeMain {
+	public static void main(String[] args) {
+		binaryTree bt=new binaryTree();
+		bt.add(1);
+		bt.add(4);
+		bt.add(12);
+		System.out.println(bt.toString());
+	}
+	
+	
+	
+}
---
+++ src/binaryTreeNode.java
@@ -0,0 +1,14 @@
+package binaryTree;
+
+public class binaryTreeNode {
+	 int value;
+	    binaryTreeNode left;
+	    binaryTreeNode right;
+
+	    binaryTreeNode(int value) {
+	        this.value = value;
+	        right = null;
+	        left = null;
+	    }
+
+}
